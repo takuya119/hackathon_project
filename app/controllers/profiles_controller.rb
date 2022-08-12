628752b9dc512196677d82821f60f7409db1631c
ky0613 <kyutech0613@gmail.com>
2022-08-13 03:22:49 +0900

    add: プロフィール機能とプロフィール画面の追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,27 @@
 class ProfilesController < ApplicationController
-  def show
-  end
+  before_action :set_user
 
-  def edit
-  end
+  def show; end
+
+  def edit; end
 
   def update
+    @user.update(user_params)
+    redirect_to profile_path
   end
 
   def destroy
+    @user.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
