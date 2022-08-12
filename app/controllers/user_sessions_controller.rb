d24248541ddb9845f5808f5b6f134b9767070672
Saeko Matsuzaki <saekorian@gmail.com>
2022-08-12 22:20:46 +0900

    ログイン機能とログインページ

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,6 +3,12 @@ class UserSessionsController < ApplicationController
   end
 
   def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
