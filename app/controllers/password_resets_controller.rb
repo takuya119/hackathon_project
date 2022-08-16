b9bf1fed9e361df8b3c3d9d783cc4f27ae9d6ec7
Saeko Matsuzaki <saekorian@gmail.com>
2022-08-16 23:42:18 +0900

    password_resets_controller修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,6 @@
 class PasswordResetsController < ApplicationController
-
-  def new
-  end
+  before_action :set_token, only: %i[edit update]
+  def new; end
 
   def create
     @user = User.find_by_email(params[:email])
@@ -10,8 +9,7 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
-    @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
+    @user = User.load_from_reset_password_token(@token)
 
     if @user.blank?
       not_authenticated
@@ -20,17 +18,20 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
-    @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
 
     return not_authenticated if @user.blank?
-
-    # githubや基礎編通りの@user.password_confirmationだとno method errorが出てしまうので、 @user.crypted_passwordにしています。
-    @user.crypted_password = params[:user][:password_confirmation]
+    @user.password_confirmation = params[:user][:password_confirmation]
     if @user.change_password(params[:user][:password])
       redirect_to login_path
     else
       render :edit
     end
+  end
+
+  private
+
+  def set_token
+    @token = params[:id]
   end
 end
