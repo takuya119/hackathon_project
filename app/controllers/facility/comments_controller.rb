2f086f9837c812780cc2d1340246d63767eea2c5
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-08-19 00:49:39 +0900

    Add: コメント投稿とバリデーションエラー時をturboで処理 エラーメッセージを表示するパーシャル作成 リプライ表示(仮)作成

--- app/controllers/facility/comments_controller.rb
+++ app/controllers/facility/comments_controller.rb
@@ -1,11 +1,14 @@
 class Facility::CommentsController < ApplicationController
   def create
-    facility = PublicFacility.find(params[:facility_id])
-    comment = facility.facility_comments.new(comment_params)
-    if comment.save
-      redirect_to facility_path(facility)
-    # else # 失敗処理は後で
-    #   render 'facility/facilities/show', status: :unprocessable_entity
+    @facility = PublicFacility.find(params[:facility_id])
+    @comment = @facility.facility_comments.new(comment_params)
+    unless @comment.save
+      # turbo_frame_tag 'comment_form'囲んだ部分からリクエストを送ると、
+      # レンダリング先にあるturbo_frame_tag 'comment_form'で囲われた部分と差し替えられる。
+
+      # render 'facility/facilities/show'でも同じ挙動になるが、その場合は
+      # @roomsや@commentsなどの変数を用意する必要がある。
+      render partial: "facility/facilities/comment_form", locals: { facility: @facility, comment: @comment, url: facility_comments_path(@facility) }
     end
   end
 
--- app/models/facility_comment.rb
+++ app/models/facility_comment.rb
@@ -2,4 +2,6 @@ class FacilityComment < ApplicationRecord
   belongs_to :public_facility
   belongs_to :room, optional: true # nullを許可するために必要
   has_many :facility_replies, dependent: :destroy
+
+  validates :content, presence: true, length: { maximum: 65_535 } # レイアウトによって調整
 end
