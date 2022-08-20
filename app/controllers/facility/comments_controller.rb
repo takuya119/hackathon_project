0229143ca7578e0849e47f8e95d45a8e4bbefd67
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-08-20 12:25:58 +0900

    Fix:  コメントのパーシャルの場所を変更

--- app/controllers/facility/comments_controller.rb
+++ app/controllers/facility/comments_controller.rb
@@ -3,13 +3,13 @@ class Facility::CommentsController < ApplicationController
     @facility = PublicFacility.find(params[:facility_id])
     @comment = @facility.facility_comments.new(comment_params)
     unless @comment.save
-      render partial: 'facility/facilities/comment_form', locals: { facility: @facility, comment: @comment, url: facility_comments_path(@facility) }
+      render partial: 'facility/comments/comment_form', locals: { facility: @facility, comment: @comment, url: facility_comments_path(@facility) }
     end
   end
 
   def show
     @comment = FacilityComment.find(params[:id])
-    render partial: 'comment', locals: { comment: @comment }
+    render partial: 'facility/comments/comment', locals: { comment: @comment }
   end
 
   def destroy # 現状コメントは消さない
