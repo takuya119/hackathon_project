cf9c4f867bf8d46efb966ff4ac205eae40522d24
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-08-20 11:47:22 +0900

    Add: コメントとリプライにバリデーションを追加

--- app/models/facility_comment.rb
+++ app/models/facility_comment.rb
@@ -3,5 +3,6 @@ class FacilityComment < ApplicationRecord
   belongs_to :room, optional: true # nullを許可するために必要
   has_many :facility_replies, dependent: :destroy
 
+  validates :name, length: { maximum: 65_535 } # レイアウトによって調整
   validates :content, presence: true, length: { maximum: 65_535 } # レイアウトによって調整
 end
--- app/models/facility_reply.rb
+++ app/models/facility_reply.rb
@@ -1,3 +1,6 @@
 class FacilityReply < ApplicationRecord
   belongs_to :facility_comment
+
+  validates :name, length: { maximum: 65_535 } # レイアウトによって調整
+  validates :content, presence: true, length: { maximum: 65_535 } # レイアウトによって調整
 end
