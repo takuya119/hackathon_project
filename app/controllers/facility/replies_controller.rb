82658debf3763e0c69395b5d4958f8018035e93e
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-08-20 12:29:10 +0900

    Fix: リプライのパーシャルの場所を変更

--- app/controllers/facility/replies_controller.rb
+++ app/controllers/facility/replies_controller.rb
@@ -8,7 +8,7 @@ class Facility::RepliesController < ApplicationController
     @comment = FacilityComment.find(params[:comment_id])
     @reply = @comment.facility_replies.new(reply_params)
     unless @reply.save
-      render partial: 'reply_form', locals: { comment: @comment, reply: @reply }
+      render partial: 'facility/replies/reply_form', locals: { comment: @comment, reply: @reply }
     end
   end
 
