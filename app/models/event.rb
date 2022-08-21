088644459ad54df1db65c7bcd44f30b52c981cfe
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-08-21 16:54:36 +0900

    データベース変更に伴いhas_many throughの定義

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,6 @@ class Event < ApplicationRecord
   has_many :event_comments, dependent: :destroy
   has_many :event_tags, dependent: :destroy
   has_many :participants, dependent: :destroy
+  has_many :tags, through: :event_tags
+  has_many :participant_users, through: :participants, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :events, dependent: :destroy
   has_many :participants, dependent: :destroy
   has_many :public_facilities, dependent: :destroy
+  has_many :bookmark_facilities, through: :bookmarks, source: :public_facility
+  has_many :participant_events, through: :participants, source: :event
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
