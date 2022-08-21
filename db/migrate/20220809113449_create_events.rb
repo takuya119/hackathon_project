31c114a0255caeab56f560f62196244acee75d08
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-08-21 14:57:16 +0900

    Change: migrationの書き換え

--- db/migrate/20220809113449_create_events.rb
+++ db/migrate/20220809113449_create_events.rb
@@ -2,11 +2,12 @@ class CreateEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :events do |t|
       t.string :name, null: false
-      t.datetime :date, null: false
+      t.datetime :start_time, null: false
+      t.datetime :end_time, null: false
       t.text :detail
       t.integer :capacity
+      t.integer :status, null: false, default: 0
       t.references :user, null: false, foreign_key: true
-      t.references :public_facility, null: false, foreign_key: true
 
       t.timestamps
     end
