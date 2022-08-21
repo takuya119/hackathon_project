3c6b2d3ef7d85a5b1adf52837d56066a55775441
takuya119 <takuya.nakahara.119@gmail.com>
2022-08-21 23:27:49 +0900

    add: イベント作成機能実装

--- app/controllers/event/events_controller.rb
+++ app/controllers/event/events_controller.rb
@@ -2,13 +2,20 @@ class Event::EventsController < ApplicationController
   def index
     @q = Event.ransack(params[:q])
     # ページネーション検討
-    @events = @q.result(distinct: true).includes(:user).order(created_at: :desc)
+    @events = @q.result(distinct: true).includes(:user).includes(:public_facility).order(created_at: :desc)
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = current_user.events.new(event_params)
+    if @event.save
+      redirect_to events_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -24,5 +31,11 @@ class Event::EventsController < ApplicationController
   end
 
   def mine
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date, :detail, :capacity, :public_facility_id)
   end
 end
