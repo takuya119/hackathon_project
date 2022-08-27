73feb2f90120c0a8f384a41cd17f5a3cc4f59b82
Saeko Matsuzaki <saekorian@gmail.com>
2022-08-27 11:14:20 +0900

    Add: Event Statusのenum追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,7 @@ class Event < ApplicationRecord
   has_many :participants, dependent: :destroy
   has_many :tags, through: :event_tags
   has_many :participant_users, through: :participants, source: :user
+
+  enum status: { 施設未予約: 0, 施設予約済み: 1, 中止: 2}
+
 end
