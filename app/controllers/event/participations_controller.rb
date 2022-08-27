40602d2a5563dfa66d624d039bec09288565ea81
Saeko Matsuzaki <saekorian@gmail.com>
2022-08-27 17:46:54 +0900

    Add: イベントへの参加、取り消し機能の追加

--- app/controllers/event/participations_controller.rb
+++ app/controllers/event/participations_controller.rb
@@ -1,7 +1,15 @@
 class Event::ParticipationsController < ApplicationController
+  before_action :require_login
+
   def create
+    @event = Event.find(params[:event_id])
+    current_user.participants.create!(event: @event)
+    redirect_to event_path(@event)
   end
 
   def destroy
+    @event = Event.find(params[:event_id])
+    current_user.participants.find_by(event_id: @event.id).destroy!
+    redirect_to event_path(params[:event_id]), status: :see_other
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_conformation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
